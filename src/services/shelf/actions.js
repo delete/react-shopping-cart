f3d6b66622f336d1a896dd19068e383b6b9779b8
delete <pinheiro.llip@gmail.com>
2019-07-01 08:50:58 -0300

    Clear code

--- src/services/shelf/actions.js
+++ src/services/shelf/actions.js
@@ -1,19 +1,6 @@
 import { FETCH_PRODUCTS } from './actionTypes';
 import firebase from '../../firebase.service';
 
-const compare = {
-  lowestprice: (a, b) => {
-    if (a.price < b.price) return -1;
-    if (a.price > b.price) return 1;
-    return 0;
-  },
-  highestprice: (a, b) => {
-    if (a.price > b.price) return -1;
-    if (a.price < b.price) return 1;
-    return 0;
-  }
-};
-
 export const fetchProducts = (filters, sortBy, callback) => async (dispatch) => {
   const productsRef = await firebase.getProducts();
   productsRef.onSnapshot((querySnapshot) => {
@@ -28,31 +15,4 @@ export const fetchProducts = (filters, sortBy, callback) => async (dispatch) => 
       payload: products
     });
   })
-  // return axios
-  //   .get(productsAPI)
-  //   .then(res => {
-  //     let { products } = res.data;
-
-  //     if (!!filters && filters.length > 0) {
-  //       products = products.filter(p =>
-  //         filters.find(f => p.availableSizes.find(size => size === f))
-  //       );
-  //     }
-
-  //     if (!!sortBy) {
-  //       products = products.sort(compare[sortBy]);
-  //     }
-
-  //     if (!!callback) {
-  //       callback();
-  //     }
-
-  //     return dispatch({
-  //       type: FETCH_PRODUCTS,
-  //       payload: products
-  //     });
-  //   })
-  //   .catch(err => {
-  //     console.log('Could not fetch products. Try again later.');
-  //   });
 };
